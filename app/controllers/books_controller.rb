82917640cda23073cf3492066fca936abbdcbb24
EC2 Default User <ec2-user@ip-172-31-34-30.ap-northeast-1.compute.internal>
2025-04-07 05:35:46 +0000

    [add] コントローラー追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def new
     @book = Book.new
   end
@@ -7,16 +8,24 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to books_path
   end 
 
   def index
+    @books = Books.all
   end
 
   def show
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update()
+    redirect_to books_path(@user.id)
   end
 
   def destroy
@@ -26,4 +35,6 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def edit
     @user =User.find(params[:id])
   end
@@ -7,12 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to users_path(@user.id)
+  end
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
   end
 
   def index
     @user = Users.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
